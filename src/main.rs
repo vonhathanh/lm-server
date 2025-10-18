2d4d506b6e56e37851c65f5962841fc0a8f0cf84
hanhvn <vonhathanh@gmail.com>
2025-10-18 16:32:54 +0700

    impl Display for Route

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,14 @@ pub mod server;
 pub mod server_utils;
 
 fn index(r: Request) -> Response {
-    !todo!()
+    println!("{:#?}", r);
+    Response("Hello".to_string())
 }
 
 fn main() {
     let mut server = Server::<Request, Response>::new();
     server.get("/".to_string(), index);
-    server.post("/user/".to_string(), index);
+    server.post("/user/logout".to_string(), index);
     server.post("/user/login".to_string(), index);
     server.put("/user/:id/name".to_string(), index);
     server.delete("/book/chaper/:id".to_string(), index);
--- src/server.rs
+++ src/server.rs
@@ -1,7 +1,5 @@
 use std::{
-    collections::HashMap,
-    io::{BufRead, BufReader, Write},
-    net::{TcpListener, TcpStream},
+    collections::HashMap, fmt::{format, write, Display}, io::{BufRead, BufReader, Write}, net::{TcpListener, TcpStream}
 };
 
 use crate::server_utils::parse_request;
@@ -11,13 +9,14 @@ const SERVER_ADDRESS: &str = "127.0.0.1:8000";
 pub trait IRequest {}
 pub trait IResponse {}
 
+#[derive(Debug)]
 pub struct Request {
     pub method: String,
     pub path_variables: Vec<String>,
     pub query_parameters: Vec<(String, String)>,
-    pub body: String
+    pub body: String,
 }
-pub struct Response(String);
+pub struct Response(pub String);
 
 impl IRequest for Request {}
 
@@ -30,17 +29,15 @@ where
     T: IRequest,
     V: IResponse,
 {
-    get_routes: RouteCollection<T, V>,
-    post_routes: RouteCollection<T, V>,
-    delete_routes: RouteCollection<T, V>,
-    put_routes: RouteCollection<T, V>,
+    get_routes: Route<T, V>,
+    post_routes: Route<T, V>,
+    delete_routes: Route<T, V>,
+    put_routes: Route<T, V>,
 }
 
-struct RouteCollection<T, V> {
-    routes: HashMap<String, Route<T, V>>,
-}
-
+#[derive(Debug)]
 struct Route<T, V> {
+    path: String,
     placeholder: Option<String>,
     handler: Option<RequestHandler<T, V>>,
     sub_routes: HashMap<String, Route<T, V>>,
@@ -49,15 +46,19 @@ struct Route<T, V> {
 impl<T: IRequest, V: IResponse> Server<T, V> {
     pub fn new() -> Self {
         Server {
-            get_routes: RouteCollection::new(),
-            post_routes: RouteCollection::new(),
-            put_routes: RouteCollection::new(),
-            delete_routes: RouteCollection::new(),
+            get_routes: Route {path: "/".to_string(), placeholder: None, handler: None, sub_routes: HashMap::new() },
+            post_routes: Route {path: "/".to_string(), placeholder: None, handler: None, sub_routes: HashMap::new() },
+            put_routes: Route {path: "/".to_string(), placeholder: None, handler: None, sub_routes: HashMap::new() },
+            delete_routes: Route {path: "/".to_string(), placeholder: None, handler: None, sub_routes: HashMap::new() },
         }
     }
 
     pub fn run(&self) {
         let listener = TcpListener::bind(SERVER_ADDRESS).unwrap();
+        println!("GETs: {}", self.get_routes);
+        println!("POSTs: {}", self.post_routes);
+        println!("PUTs: {}", self.put_routes);
+        println!("DELETEs: {}", self.delete_routes);
         println!("Server is listening at: {SERVER_ADDRESS}");
         for stream in listener.incoming() {
             let tcp_stream = stream.unwrap();
@@ -66,19 +67,19 @@ impl<T: IRequest, V: IResponse> Server<T, V> {
     }
 
     pub fn get(&mut self, path: String, handler: RequestHandler<T, V>) {
-        self.get_routes.add(path, handler);
+        self.get_routes.add(path.split_terminator('/').collect(), handler);
     }
 
     pub fn post(&mut self, path: String, handler: RequestHandler<T, V>) {
-        self.post_routes.add(path, handler);
+        self.post_routes.add(path.split_terminator('/').collect(), handler);
     }
 
     pub fn put(&mut self, path: String, handler: RequestHandler<T, V>) {
-        self.put_routes.add(path, handler);
+        self.put_routes.add(path.split_terminator('/').collect(), handler);
     }
 
     pub fn delete(&mut self, path: String, handler: RequestHandler<T, V>) {
-        self.delete_routes.add(path, handler);
+        self.delete_routes.add(path.split_terminator('/').collect(), handler);
     }
 
     fn handle_connection(&self, mut stream: TcpStream) {
@@ -111,7 +112,7 @@ impl<T: IRequest, V: IResponse> Server<T, V> {
         // }
     }
 
-    fn match_request(&self, request: &Request){
+    fn match_request(&self, request: &Request) {
         if request.method == "GET" {
             self.get_routes.handle(request);
         } else if request.method == "POST" {
@@ -126,50 +127,15 @@ impl<T: IRequest, V: IResponse> Server<T, V> {
     }
 }
 
-impl<T, V> RouteCollection<T, V> {
-    fn new() -> Self {
-        RouteCollection {
-            routes: HashMap::new(),
-        }
-    }
-
-    fn add(&mut self, path: String, handler: RequestHandler<T, V>) {
-        let mut tokens: Vec<&str> = path.split_terminator('/').collect();
-        let root = tokens[0];
-        let mut placeholder: Option<String> = None;
-
-        if root.find(':') == Some(1) {
-            placeholder = Some(root[1..].to_string());
-        }
-        if self.routes.contains_key(root) {
-            let route = self.routes.get_mut(root).unwrap();
-            if tokens.len() > 1 {
-                route.add(tokens.split_off(1), handler);
-            }
-        } else {
-            let mut route ;
-            if tokens.len() == 1 {
-                route = Route::new(placeholder, Some(handler), HashMap::new());
-            } else {
-                route = Route::new(placeholder, None, HashMap::new());
-            }
-            route.add(tokens.split_off(1), handler);
-            self.routes.entry(root.to_string()).or_insert(route);
-        }
-    }
-
-    fn handle(&self, request: &Request) {
-
-    }
-}
-
 impl<T, V> Route<T, V> {
     fn new(
+        path: String,
         placeholder: Option<String>,
         handler: Option<RequestHandler<T, V>>,
         sub_routes: HashMap<String, Route<T, V>>,
     ) -> Self {
         Route {
+            path,
             placeholder,
             handler,
             sub_routes,
@@ -181,27 +147,58 @@ impl<T, V> Route<T, V> {
             return;
         }
 
+        if tokens[0] == "".to_string() && tokens.len() > 1 {
+            tokens = tokens.split_off(1);
+        }
+
+        // skip the first token if it's the index root
         let root = tokens[0];
         let mut placeholder: Option<String> = None;
 
+        // index root (/) detected, just need to update handler function
+        if self.path == "/".to_string() && tokens.len() == 1 {
+            self.handler = Some(handler);
+            return;
+        }
+
+        // placeholder detection likes :id, :name...
         if root.find(':') == Some(0) {
             placeholder = Some(root[1..].to_string());
         }
 
+        // this route is already added
         if self.sub_routes.contains_key(root) {
-            let route = self.sub_routes.get_mut(root).unwrap();
+            // not the end of the full route, delegate the add route task to it's corresponding sub route.
             if tokens.len() > 1 {
+                let route = self.sub_routes.get_mut(root).unwrap();
                 route.add(tokens.split_off(1), handler);
             }
         } else {
-            let mut route ;
+            let path = format!("{}{}/", self.path, root);
+            let mut route;
             if tokens.len() == 1 {
-                route = Route::new(placeholder, Some(handler), HashMap::new());
+                route = Route::new(path, placeholder, Some(handler), HashMap::new());
             } else {
-                route = Route::new(placeholder, None, HashMap::new());
+                route = Route::new(path, placeholder, None, HashMap::new());
             }
             route.add(tokens.split_off(1), handler);
             self.sub_routes.entry(root.to_string()).or_insert(route);
         }
+    }
+
+    fn handle(&self, request: &Request) {}
+}
+
+impl<T, V> Display for Route<T, V> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        if self.sub_routes.len() == 0{
+            write!(f, "{}\n", self.path).unwrap();
+        } 
+
+        for (_, route) in &self.sub_routes {
+            route.fmt(f).unwrap();
+        }
+        
+        Ok(())
     }
 }
