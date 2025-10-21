34e219b36ad327992c6602f25d8f05a26dd26539
hanhvn <vonhathanh@gmail.com>
2025-10-21 20:34:36 +0700

    uncomment  code in main

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,15 @@ pub mod server_utils;
 
 fn index(r: &dyn IRequest) -> Box<dyn IResponse> {
     println!("{}", r.get_method());
-    Box::new(Response("Hello".to_string()))
+    Box::new(Response("Hello world!".to_string()))
 }
 
 fn main() {
     let mut server = Server::new();
     server.get("/".to_string(), index);
-    // server.post("/user/logout".to_string(), index);
-    // server.post("/user/login".to_string(), index);
-    // server.put("/user/:id/name".to_string(), index);
-    // server.delete("/book/chaper/:id".to_string(), index);
+    server.post("/user/logout".to_string(), index);
+    server.post("/user/login".to_string(), index);
+    server.put("/user/:id/name".to_string(), index);
+    server.delete("/book/chaper/:id".to_string(), index);
     server.run();
 }
