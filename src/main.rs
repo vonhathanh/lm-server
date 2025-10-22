3e95d5c7c4a5c7c323331142ced3e04a18025cd5
hanhvn <vonhathanh@gmail.com>
2025-10-23 06:27:27 +0700

    test /login API

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,10 @@ use crate::server::{IRequest, IResponse, Response, Server};
 
 pub mod server;
 pub mod server_utils;
+pub mod auth;
 
 // must return Box<T> because trait object is dynamically sized type, so we wrap it inside a smart pointer
-fn index(r: &dyn IRequest) -> Box<dyn IResponse> {
-    println!("{}", r.get_method());
+fn index(_r: &dyn IRequest) -> Box<dyn IResponse> {
     Box::new(Response("Hello world!".to_string()))
 }
 
@@ -13,7 +13,7 @@ fn main() {
     let mut server = Server::new();
     server.get("/", index);
     server.post("/user/logout", index);
-    server.post("/user/login", index);
+    server.post("/user/login", auth::login);
     server.put("/user/:id/name", index);
     server.delete("/book/chaper/:id", index);
     server.run();
